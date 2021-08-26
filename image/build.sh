f97ed0b0315d8c9cf40dcc0175b0c278f8db9e05
koyamanX <ckoyama.1996@gmail.com>
2021-08-26 16:04:34 +0900

    Add Dockerfile

---
+++ image/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t rv32xooo_dev:v1.0 .
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -it --rm \
+    --net host \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --privileged \
+    -v /sys:/sys:ro \
+    -v /opt/intelFPGA_lite/20.1:/opt/intelFPGA_lite/latest \
+    -v $(pwd)/core:/root/core \
+	rv32xooo_dev:v1.0 $1
